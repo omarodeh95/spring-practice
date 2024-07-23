21406f7793f44425665bec6d00fe9e66778256c1
Omar Odeh <omar.odeh95@hotmail.com>
2024-07-24 02:41:43 +0300

    Init jdbc spring app

---
+++ spring-jdbc/jdbc/src/main/java/dev/omar_learning/jdbc/JdbcApplication.java
@@ -0,0 +1,13 @@
+package dev.omar_learning.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApplication.class, args);
+	}
+
+}
---
+++ spring-jdbc/jdbc/src/test/java/dev/omar_learning/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.omar_learning.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
