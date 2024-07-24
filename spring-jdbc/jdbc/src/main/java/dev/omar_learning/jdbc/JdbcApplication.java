fee1c9bf9f75577f43a7d35ced183db62a7c4bdf
Omar Odeh <omar.odeh95@hotmail.com>
2024-07-24 03:20:21 +0300

    Connect to h2 db and jdbc template

--- spring-jdbc/jdbc/src/main/java/dev/omar_learning/jdbc/JdbcApplication.java
+++ spring-jdbc/jdbc/src/main/java/dev/omar_learning/jdbc/JdbcApplication.java
@@ -1,13 +1,31 @@
 package dev.omar_learning.jdbc;
 
+
+import javax.sql.DataSource;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import lombok.extern.java.Log;
 
 @SpringBootApplication
-public class JdbcApplication {
+@Log
+public class JdbcApplication implements CommandLineRunner {
+
+  private final DataSource dataSource;
+
+  public JdbcApplication(final DataSource dataSource) { this.dataSource = dataSource; }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
 	}
 
+  @Override
+  public void run(final String... args) {
+    log.info("Datasource: " + dataSource.toString());
+    final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+    restTemplate.execute("SELECT 1");
+  }
+
 }
+
