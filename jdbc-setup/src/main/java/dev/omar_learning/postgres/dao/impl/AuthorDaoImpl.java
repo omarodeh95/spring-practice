3bdc65228e91d6749fb6f92dd98face61efa54de
Omar Odeh <omar.odeh95@hotmail.com>
2024-07-24 21:38:10 +0300

    Add implementation for author dao create method

--- jdbc-setup/src/main/java/dev/omar_learning/postgres/dao/impl/AuthorDaoImpl.java
+++ jdbc-setup/src/main/java/dev/omar_learning/postgres/dao/impl/AuthorDaoImpl.java
@@ -3,6 +3,7 @@ package dev.omar_learning.postgres.dao.impl;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import dev.omar_learning.postgres.dao.AuthorDao;
+import dev.omar_learning.postgres.domain.Author;
 
 
 public class AuthorDaoImpl implements AuthorDao {
@@ -11,5 +12,10 @@ public class AuthorDaoImpl implements AuthorDao {
 
   public AuthorDaoImpl(final JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @Override
+  public void create(Author author) {
+    jdbcTemplate.update("INSERT INTO authors (id, name, age) VALUES (?, ?, ?)", author.getId(), author.getName(), author.getAge());
   }
 }
