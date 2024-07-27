94d8b72de5bf48e7cc2001096b7d21026ce294b1
Omar Odeh <omar.odeh95@hotmail.com>
2024-07-27 07:14:59 +0300

    Fix Test Data util to accept author object

--- jpa-setup/src/test/java/dev/omar_learning/postgres/TestDataUtil.java
+++ jpa-setup/src/test/java/dev/omar_learning/postgres/TestDataUtil.java
@@ -1,78 +1,77 @@
-// package dev.omar_learning.postgres;
-//
-// import dev.omar_learning.postgres.domain.Author;
-// import dev.omar_learning.postgres.domain.Book;
-//
-// public class TestDataUtil {
-//
-//   // Should we make this an abstract class?
-//   private TestDataUtil(){
-//   }
-//
-//   public static Author buildTestAuthor() {
-//     return Author.builder()
-//       .id(1L)
-//       .name("Omar Odeh")
-//       .age(90)
-//       .build();
-//   }
-//
-//   // This is not clean and should be configurable using the the builTestAuthor parameters
-//   public static Author buildTestAuthorA() {
-//     return Author.builder()
-//       .id(1L)
-//       .name("Omar Odeh")
-//       .age(90)
-//       .build();
-//   }
-//
-//   public static Author buildTestAuthorB() {
-//     return Author.builder()
-//       .id(2L)
-//       .name("Omar Odeh")
-//       .age(90)
-//       .build();
-//   }
-//
-//   public static Author buildTestAuthorC() {
-//     return Author.builder()
-//       .id(3L)
-//       .name("Omar Odeh")
-//       .age(90)
-//       .build();
-//   }
-//
-//   public static Book buildTestBook() {
-//     return Book.builder()
-//       .isbn("123-456-789")
-//       .title("Lord of The Ring")
-//       .authorId(1L)
-//       .build();
-//   }
-//
-//   // This is not clean and should be configurable using the the builTestAuthor parameters
-//   public static Book buildTestBookA() {
-//     return Book.builder()
-//       .isbn("123-456-78910")
-//       .title("Lord of The Ring")
-//       .authorId(1L)
-//       .build();
-//   }
-//
-//   public static Book buildTestBookB() {
-//     return Book.builder()
-//       .isbn("123-456-78911")
-//       .title("Lord of The Ring")
-//       .authorId(2L)
-//       .build();
-//   }
-//
-//   public static Book buildTestBookC() {
-//     return Book.builder()
-//       .isbn("123-456-78912")
-//       .title("Lord of The Ring")
-//       .authorId(3L)
-//       .build();
-//   }
-// }
-//
+package dev.omar_learning.postgres;
+
+import dev.omar_learning.postgres.domain.Author;
+import dev.omar_learning.postgres.domain.Book;
+
+public class TestDataUtil {
+
+  private TestDataUtil(){
+  }
+
+  public static Author buildTestAuthor() {
+    return Author.builder()
+      .id(1L)
+      .name("Omar Odeh")
+      .age(90)
+      .build();
+  }
+
+  // This is not clean and should be configurable using the the builTestAuthor parameters
+  public static Author buildTestAuthorA() {
+    return Author.builder()
+      .id(1L)
+      .name("Omar Odeh")
+      .age(90)
+      .build();
+  }
+
+  public static Author buildTestAuthorB() {
+    return Author.builder()
+      .id(2L)
+      .name("Omar Odeh")
+      .age(90)
+      .build();
+  }
+
+  public static Author buildTestAuthorC() {
+    return Author.builder()
+      .id(3L)
+      .name("Omar Odeh")
+      .age(90)
+      .build();
+  }
+
+  public static Book buildTestBook(Author author) {
+    return Book.builder()
+      .isbn("123-456-789")
+      .title("Lord of The Ring")
+      .author(author)
+      .build();
+  }
+
+  // This is not clean and should be configurable using the the builTestAuthor parameters
+  public static Book buildTestBookA(Author author) {
+    return Book.builder()
+      .isbn("123-456-78910")
+      .title("Lord of The Ring")
+      .author(author)
+      .build();
+  }
+
+  public static Book buildTestBookB(Author author) {
+    return Book.builder()
+      .isbn("123-456-78911")
+      .title("Lord of The Ring")
+      .author(author)
+      .build();
+  }
+
+  public static Book buildTestBookC(Author author) {
+    return Book.builder()
+      .isbn("123-456-78912")
+      .title("Lord of The Ring")
+      .author(author)
+      .build();
+  }
+}
+
