7e9e08da30369c820d05bcc946da8a47954e6ab9
Omar Odeh <omar.odeh95@hotmail.com>
2024-07-30 08:35:58 +0300

    Inject Newtopic to the main class, temporarily

--- kafka/library-events-producer/src/main/java/dev/learnkafka/library_events_producer/LibraryEventsProducerApplication.java
+++ kafka/library-events-producer/src/main/java/dev/learnkafka/library_events_producer/LibraryEventsProducerApplication.java
@@ -3,11 +3,25 @@ package dev.learnkafka.library_events_producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.beans.factory.annotation.Value;
+
 @SpringBootApplication
 public class LibraryEventsProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryEventsProducerApplication.class, args);
 	}
+
+  // FIXME: This following could shoe be auto scaned when loaded from the AuthorCreateConfiguration file, but for some reason it wasn't wokring
+  @Value("${spring.kafka.topic}")
+  private String topic;
+
+  @Bean
+  public NewTopic libraryEvents() {
+    return TopicBuilder.name(topic).partitions(3).replicas(3).build();
+  }
 
 }
